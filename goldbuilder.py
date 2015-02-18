0c4e4adba3d326fe22cbc1f7905692b214862cbc
dboth <github@dboth.de>
2015-02-18 17:10:51 +0100

    dsc

--- goldbuilder.py
+++ goldbuilder.py
@@ -34,7 +34,7 @@ with open(inputname) as fileobject:
 		i = 1
 		for synset in wn.synsets(verb1,pos=wn.VERB):
 			print str(i)+": "+synset.definition()
-			print "Examples: "+synset.examples()[0]
+			print "Examples: "+" - ".join(synset.examples())
 			zuordnung[i] = synset.name()
 			i+=1
 		sensea = input("Correct Sense (Number): ")	
@@ -45,7 +45,7 @@ with open(inputname) as fileobject:
 		i = 1
 		for synset in wn.synsets(verb2,pos=wn.VERB):
 			print str(i)+": "+synset.definition()
-			print "Examples: "+synset.examples()[0]
+			print "Examples: "+" - ".join(synset.examples())
 			zuordnung[i] = synset.name()
 			i+=1
 		sensea = input("Correct Sense (Number): ")	
