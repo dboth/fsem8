c05ddb54566929de479b4d2839dcb62dbc01d5ba
dboth <github@dboth.de>
2015-03-14 14:01:13 +0100

    ontonotes ´kommentiert

--- dictionarybuilder/ontonotesreader.py
+++ dictionarybuilder/ontonotesreader.py
@@ -5,13 +5,15 @@
 #add column lang_id to sentence
 #fill lang_id by getting the value out of document using key sentence.document_id on document.id
 
+#load mysql.connector, nltk.corpus.wordnet, xml.minidom, sys.stdout, json, time and os
 import mysql.connector as cn
 from nltk.corpus import wordnet as wn
 from xml.dom import minidom
 from sys import stdout
 import json
-import time,os
+import time, os
 
+#function takes a list of synsets, counts their cooccurence and adds it to the given dictionary
 def addSentenceToDictionary(synsets, dictionary):
 		num_sets = len(synsets)
 		for x in range(num_sets): #last element does not have to be called, because it has no partner
@@ -22,20 +24,24 @@ def addSentenceToDictionary(synsets, dictionary):
 				else:
 					dictionary[key] = 1
 		return dictionary
-		
+
+#init dictionary and mysql connection		
 dictionary = {}
 cd = cn.connect(user="root", password="root", host='127.0.0.1', database='ontonotes',buffered=True)
 cnx = cd.cursor()
+#select all english sentences. for this query the lang_id column must have been manually added to the sentence table!
 query = "SELECT id FROM sentence WHERE lang_id = 'en'"
 cnx.execute(query)
+#initialize the clock getting a remaining time counter
 c = 1
 begintime = time.clock()
-
+#iterate through all sentences
 for (id) in cnx:
+	#calculate remaining time
 	now = time.clock()
 	vg = now-begintime
 	os.system('cls' if os.name == 'nt' else 'clear')
-	print("Satz "+str(c)+" von 115.812")
+	print("Satz "+str(c)+" von 115.812") #115812 is the number of english sentences in the ontonotes database. this has to be changed if another ontonotes db is used.
 	seconds = vg/(float(c)/115812*100)*100
 	#subtract elapsed time to get remaining time
 	seconds -= vg
@@ -44,14 +50,17 @@ for (id) in cnx:
 	h, m = divmod(m, 60)
 	#print remaining 
 	print "Remaining: %02d:%02d:%02d hours" % (h, m, s)
+	#get all senses of the words in this sentence out of the on_sense table.
 	cnx2 = cd.cursor()
 	query = "SELECT lemma, sense FROM on_sense WHERE id LIKE '%"+id[0]+"' AND pos = 'v'"
 	cnx2.execute(query)
 	senselist = []
+	#iterate through sentence senses
 	for (lemma, sense) in cnx2:
-		#print lemma, sense
+		#get the sense information out of ontonote's sense inventory files
 		xmldoc = minidom.parse("sense-inventories/"+lemma+"-v.xml")
 		itemlist = xmldoc.getElementsByTagName('sense')
+		#search the wordnet sense in the xml model
 		for s in itemlist:
 			if (s.attributes['n'].value == sense):
 				for i in s.getElementsByTagName('wn'):
@@ -60,13 +69,14 @@ for (id) in cnx:
 							senses = i.firstChild.nodeValue.split(",")
 							for sens in senses:
 								senselist += [wn.synset(lemma+".v."+sens).name()]
+	#add the senselist to the dictionary
 	dictionary = addSentenceToDictionary(sorted(senselist),dictionary)					
 	cnx2.close()
 	c+=1
-	
+#close the mysql connections
 cnx.close()
 cd.close()
-
+#save the dictionary as json
 f = open("ontonote.json","w")
 dct = json.dump(dictionary,f)
 f.close()
